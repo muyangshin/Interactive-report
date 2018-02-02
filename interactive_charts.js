540ececa6454caa90fbc561dddb894be122f05dc
muyang <muyang@BERKELEY.EDU>
2018-02-02 15:22:07 -0800

    Add data label formatting: time

--- interactive_charts.js
+++ interactive_charts.js
@@ -10,7 +10,7 @@ example: loadBarChart('data/df_sacm_by_concern_type.json', 'Figure 1: Number of 
 	chart_x: Categorical variable used for this bar chart;
 	chart_values: Variable(s) that we are interested in; even if it is a single variable, it should be passed as an array
 */
-function loadBarChart (dataset, chart_title, chart_x, chart_values) {
+function loadBarChart (dataset, chart_title, chart_x, chart_values, label_format = null) {
 	// colors
 	var chart_colors = {};
 	chart_values.forEach(function(e, i) {
@@ -28,6 +28,32 @@ function loadBarChart (dataset, chart_title, chart_x, chart_values) {
 		},
 		type: 'bar',
 		colors: chart_colors,
+		done: function() {
+			if (label_format != null) {
+				// data label
+				switch(label_format) {
+					// time: 2.5 -> 2h 30m
+					case 'time':
+						d3.select('.c3-chart-texts')
+							.selectAll('.c3-texts')
+							.each(function() {
+								d3.select(this)
+									.selectAll('text')
+									.each(function(d, i) {
+										var value_old = +d3.select(this).text();
+										var h = Math.floor(value_old);
+										var m = Math.floor((value_old - h) * 60);
+										var value_new = (h > 0 ? h + 'h ' + m + 'm' : m + 'm');
+										d3.select(this).text(value_new);
+									});
+							});
+						break;
+					default:
+						alert('Incorrect label format name');
+				}
+				
+			}
+		}
 	});
 
 	// title
