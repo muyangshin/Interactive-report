743b914580da16bc96ebc54c18f833befa978b67
muyang <muyang@BERKELEY.EDU>
2018-02-05 11:18:32 -0800

    Add stacked bar chart, Add better examples

--- js/interactive_charts.js
+++ js/interactive_charts.js
@@ -3,17 +3,19 @@ var calPalette = ['#003262', '#FDB515', '#B9D3B6', '#D9661F', '#00B0DA', '#DDD5C
 
 /* 
 loads a bar chart
-example: loadBarChart('data/df_sacm_by_concern_type.json', 'Figure 1: Number of Cases', 'concern_type', ['number_of_cases']);
+example: loadBarChart('data/bar_example.json', 'Bar Chart Example', 'concern_type', ['number_of_cases']);
 
 	dataset: File path of the JSON file
 	chart_title: Title of this chart
-	chart_x: Categorical variable used for this bar chart;
-	chart_values: Variable(s) that we are interested in; even if it is a single variable, it should be passed as an array
+	var_x: Categorical variable used for this bar chart;
+	var_ys: Variable(s) that we are interested in; even if it is a single variable, it should be passed as an array
+	label_format (optional): currently only supports time
+		time: 2.5 -> 2h 30m
 */
-function loadBarChart (dataset, chart_title, chart_x, chart_values, label_format = null) {
+function loadBarChart (dataset, chart_title, var_x, var_ys, label_format = null) {
 	// colors
 	var chart_colors = {};
-	chart_values.forEach(function(e, i) {
+	var_ys.forEach(function(e, i) {
 		chart_colors[e] = calPalette[i];
 	})
 	
@@ -23,13 +25,17 @@ function loadBarChart (dataset, chart_title, chart_x, chart_values, label_format
 		url: dataset,
 		mimeType: 'json',
 		keys: {
-			x: chart_x,
-			value: chart_values
+			x: var_x,
+			value: var_ys
 		},
 		type: 'bar',
 		colors: chart_colors,
 		done: function() {
 			if (label_format != null) {
+				// show y axis
+				d3.select('.c3-axis.c3-axis-y')
+					.style("visibility", "visible");
+					
 				// data label
 				switch(label_format) {
 					// time: 2.5 -> 2h 30m
@@ -61,22 +67,104 @@ function loadBarChart (dataset, chart_title, chart_x, chart_values, label_format
 }
 
 /* 
-loads a pie chart
-example: loadPieChart('data/df_sacm_crossover.json', 'Figure 3: Percentage of Transferred Cases', 'crossover', 'percentage');
+loads a stacked bar chart. Assumes numbers add up to 1, use percentage as default.
+example: loadStackedBarChart('data/bar_example.json', 'Stacked Bar Example', 'type', 'percentage', 'response');
 
 	dataset: File path of the JSON file
 	chart_title: Title of this chart
-	chart_x: Categorical variable used for this bar chart;
-	chart_values: Variable that we are interested in; 
+	var_x: Categorical variable used for this bar chart (for example, type: on-campus/off-campus)
+	var_y: Variable that we are interested in (for example, percentage); numbers should add up to 1
+	var_group: Variable to group by (for example, response: satisfied/dissatisfied)
 */
-function loadPieChart (dataset, chart_title, chart_x, chart_value) {
+function loadStackedBarChart (dataset, chart_title, var_x, var_y, var_group, label_format = null) {
 	$.getJSON(dataset, function(jsonData) {
-		// pie chart needs a slightly different data format. construct c3js-readable data/categories
+		// converts narrow data to wide
+		// use an object as an intermediate step
+		var obj_data = {};
+		var categories = [];	// categories for var_group
+		jsonData.forEach(function(e) {
+			if (e[var_x] in obj_data) {
+				obj_data[e[var_x]][e[var_group]] = e[var_y];
+			} else {
+				obj_data[e[var_x]] = {
+					[var_x]: e[var_x],
+					[e[var_group]]: e[var_y]
+				};
+			}
+			
+			if (!(categories.includes(e[var_group]))) {
+				categories.push(e[var_group]);
+			}
+		});
+		
+		// construct data array
+		var data = [];
+		Object.keys(obj_data).forEach(function(e) {
+			data.push(obj_data[e]);
+		});
+		
+		// colors
+		var chart_colors = {};
+		categories.forEach(function(e, i) {
+			chart_colors[e] = calPalette[i];
+		})
+	
+		// reload chart
+		chart.load({
+			unload: true,
+			json: data,
+			keys: {
+				x: var_x,
+				value: categories
+			},
+			type: 'bar',
+	 		colors: chart_colors,
+			done: function() {
+				// grouping
+				chart.groups([categories]);
+	
+				// hide y axis
+				d3.select('.c3-axis.c3-axis-y')
+					.style("visibility", "hidden");
+				
+				// data label
+				d3.select('.c3-chart-texts')
+					.selectAll('.c3-texts')
+					.each(function() {
+						d3.select(this)
+							.selectAll('text')
+							.each(function(d, i) {
+								var value_old = +d3.select(this).text();
+								var value_new = Math.round(value_old * 10000) / 100 + '%';
+								d3.select(this).text(value_new);
+							});
+					});
+				
+			} 
+		});
+
+		// title
+		d3.select('.c3-title').node().innerHTML = chart_title;
+	});
+}
+
+/* 
+loads a pie chart
+example: loadPieChart('data/pie_example.json', 'Pie Chart Example', 'crossover', 'percentage');
+
+	dataset: File path of the JSON file
+	chart_title: Title of this chart
+	var_x: Categorical variable used for this bar chart;
+	var_ys: Variable that we are interested in; 
+*/
+function loadPieChart (dataset, chart_title, var_x, var_y, label_format = null) {
+	$.getJSON(dataset, function(jsonData) {
+		// converts narrow data to wide
 		var data = {};
 		var categories = [];
 		jsonData.forEach(function(e) {
-			categories.push(e[chart_x]);
-			data[e[chart_x]] = e[chart_value];
+			categories.push(e[var_x]);
+			data[e[var_x]] = e[var_y];
 		});
 
 		// colors
